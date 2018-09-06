61550a5664fe90b8565aa5f41a5cebe06bba5c19
Carsten Dimmek <carsten.dimmek@irian.eu>
2018-09-06 13:16:56 +0200

    Docker aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,50 +1,20 @@
 . ~/.config/fish/aliases.fish
 
 
-function fish_prompt
-    set last_status $status
+function dotfiles
+    git pull ~/.dotfiles
+    stow -d ~/.dotfiles (ls -1  -d ~/.dotfiles/*/ | tr '\n' '\0' | xargs -0 -n 1 basename)
+end
 
-    echo ' '
 
-    set user (whoami)
+function dri
+    docker rmi $@ (docker images -q);
+end
 
-    set_color magenta
-    printf '%s' $user
-    set_color normal
-    printf ' at '
+function drc
+    docker rm (docker ps -q -a);
+end
 
-    set_color yellow
-    printf '%s' (hostname -s)
-    set_color normal
-    printf ' in '
-
-    set_color $fish_color_cwd
-    printf '%s' (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
-    set_color normal
-
-    git_prompt
-    virtualenv_prompt
-
-    set_color normal
-    printf ' (%s)' (date +%H:%M)
-    echo
-
-    #if test $last_status -eq 0
-    #    set_color white -o
-    #    printf '><((°> '
-    #else
-    #    set_color red -o
-    #    printf '><((ˣ> '
-    #    set_color white -o
-    #    printf '[%d] ' $last_status
-    #end
-
-    if test $last_status -ne 0
-        set_color white -o
-        printf '[%d] ' $last_status
-        set_color normal
-    end
-    printf '$ '
-
-    set_color normal
+function drv
+    docker volume rm (docker volume ls -q)
 end
